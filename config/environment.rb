b1c3bd3b4c7dcc2380303d40669fda9c1253d417
Aditi Parihar <aditiparihar06@gmail.com>
2024-06-27 19:19:52 +0530

    add flash messages and errors

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+# How to disable Rails Form's `.field_with_errors
+ActionView::Base.field_error_proc = proc do |html_tag, instance|
+  html_tag.html_safe
+end
