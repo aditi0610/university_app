e775afa0bbf4e65012ac87fa2e30bb01cd75372b
Aditi Parihar <aditiparihar06@gmail.com>
2024-07-05 10:28:42 +0530

    create and destroy sessions-add auth method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	def session_link
+		if (logged_in?) 
+         link_to("logout", logout_path, method: :delete)
+        else 
+         link_to("login", login_path)
+        end 
+		
+	end
 end
+
+#whenever we put in our helpers application helper is available to all our views to use.
